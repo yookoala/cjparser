0bf208620d395ac99877ca2038c49fa689d694f9
Koala Yeung <koalay@gmail.com>
2013-12-07 04:02:27 +0800

    Finish parseCangjie5File() in helpers.go
    Finished the function to parse cangjie 5 data files.

--- src/helpers.go
+++ src/helpers.go
@@ -5,8 +5,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	//"log"
+	"log"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 // Read a whole file into the memory and store it as array of lines
@@ -41,34 +43,52 @@ func readLines(path string) (lines []string, err error) {
 	return
 }
 
-func parseCangjie5File(filename string, handlerName string, db *ConvertDB) {
-	//tx, err := db.DB.Begin()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer tx.Commit()
+func parseCangjie5File(filename string, handlerName string, cat string, db *ConvertDB) {
+
+	var (
+		sect   string
+		serial uint32
+	)
+
+	tx, err := db.DB.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Commit()
 	lines, err := readLines(filename)
 	if err != nil {
 		panic(err)
 	}
 	for _, line := range lines {
-		fmt.Println(line)
-		/*
-			item, err := parseLine(line)
-			if err != nil {
-				panic(err)
+		line = strings.Trim(line, "\t ")
+		if line[0] == '[' && line[len(line)-1] == ']' {
+			sect = line[1 : len(line)-1]
+		} else if sect == "Text" && len(line) != 0 {
+			serial += 1
+			splited := strings.SplitN(line, "", 2)
+			unicode, _ := utf8.DecodeRuneInString(splited[0])
+			item := cangjieValue{
+				Unicode:   fmt.Sprintf("U+%x", unicode),
+				Character: splited[0],
+				Version:   handlerName,
+				Category:  cat,
+				Code:      splited[1],
+				Serial:    serial,
 			}
 			err = db.Insert(handlerName, tx, item)
 			if err != nil {
 				panic(err)
 			}
-		*/
+
+		}
 	}
 }
 
 type cangjieValue struct {
 	Unicode   string
 	Character string
+	Version   string
+	Category  string
 	Code      string
 	Serial    uint32
 }
